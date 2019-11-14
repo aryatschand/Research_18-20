d98f8c3b3394b80f582574e0890aa58ffc7ad84f
aryatschand <43081871+aryatschand@users.noreply.github.com>
2019-11-14 18:04:13 -0500

    SQL read/write

---
+++ sqlRead.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import pymysql
+
+# Open database connection
+db = pymysql.connect("localhost","root","arya123","pythonTest" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Prepare SQL query to INSERT a record into the database.
+sql = "SELECT * FROM potluck"
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for x in range(0,len(results)):
+      print("id={}, name={}, food={}, confirm={}, data={}".format(results[x][0], results[x][1], results[x][2], results[x][3], results[x][4]))
+except:
+   print ("Error: unable to fetch data")
+
+# disconnect from server
+db.close()
