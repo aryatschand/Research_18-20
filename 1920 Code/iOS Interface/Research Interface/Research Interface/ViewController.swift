601e48efb8c6df02482e655ef6b2a48191d53134
Arya Tschand <43081871+aryatschand@users.noreply.github.com>
2019-12-31 13:18:34 -0500

    Create xcode project for interface

---
+++ 1920 Code/iOS Interface/Research Interface/Research Interface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Research Interface
+//
+//  Created by Arya Tschand on 12/31/19.
+//  Copyright © 2019 aryatschand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
