f0627910ae94183975eaf3cdc81515ef15a6ad8e
Arya Tschand <43081871+aryatschand@users.noreply.github.com>
2020-02-03 17:11:47 -0500

    Drone code update for RFID location functionality

--- 1920 Code/Drone/datainput.py
+++ 1920 Code/Drone/datainput.py
@@ -3,6 +3,7 @@ import base64
 import requests
 import RPi.GPIO as GPIO
 import Adafruit_CharLCD as LCD
+from mfrc522 import SimpleMFRC522
 import time
 import sys
 import Adafruit_DHT
@@ -10,7 +11,7 @@ import sys
 
 ip = sys.argv[1]
 
-lcd_rs = 25
+lcd_rs = 16
 lcd_en = 24
 lcd_d4 = 23
 lcd_d5 = 21
@@ -22,7 +23,14 @@ lcd_backlight = 2
 lcd_columns = 16
 lcd_rows = 2
 
+rfid = ""
+reader = SimpleMFRC522()
+
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+
+plant_num = ""
+xLocation = ""
+yLocation = ""
 
 while True:
     time.sleep(1.0)
@@ -33,13 +41,41 @@ while True:
     mystring1 = ""
     lcd.clear()
     lcd.message('Converting\nTo B64')
+    
     with open("/home/pi/test/Images/image.jpg", "rb") as img_file1:
         my_string1 = base64.b64encode(img_file1.read())
+    
     response = requests.get("http://"+ ip + ":5000/?usage=demo&image=" + str(my_string1))
     time.sleep(1.0)
     lcd.clear()
     lcd.message('Response:\n' + response.text)
+    
     if response.text == "Demo":
+        lcd.clear()
+        lcd.message('Finding\nRFID Tag')
+        
+        while (rfid == ""):
+            try:
+                id, text = reader.read()
+            finally:
+                rfid = str(id)
+                
+        time.sleep(0.2)
+        querystring = "http://"+ ip + ":5000/?usage=rfid&tag="
+        querystring+= str(rfid)
+        response = requests.get(querystring)
+        response = response.text.split(",")
+        plant_num = response[0]
+        xLocation = response[1]
+        yLocation = response[2]
+        
+        lcd.clear()
+        lcd.message('Tag Number\n' + str(id))
+        time.sleep(1.0)
+        lcd.clear()
+        lcd.message('X Coord: ' + str(xLocation) + '\nY Coord: ' + str(yLocation))
+        time.sleep(1.0)
+
         lcd.clear()
         lcd.message('Measuring\nTemperature')
         temp=20
@@ -51,6 +87,7 @@ while True:
             temp = int(temperature)
         lcd.clear()
         lcd.message('Measuring\nLight')
+        
         light=20
         mpin=17
         tpin=27
@@ -59,6 +96,7 @@ while True:
         adj=2.130620985
         i=0
         t=0
+        
         while light == 20:
             GPIO.setup(mpin, GPIO.OUT)
             GPIO.setup(tpin, GPIO.OUT)
@@ -70,6 +108,7 @@ while True:
             GPIO.output(tpin, True)
             starttime=time.time()
             endtime=time.time()
+            
             while (GPIO.input(mpin) == GPIO.LOW):
                 endtime=time.time()
             measureresistance=endtime-starttime
@@ -77,13 +116,14 @@ while True:
             res=(measureresistance/cap)*adj
             i=i+1
             t=t+res
+            
             if i==10:
                     t=t/i
                     print(t)
                     light = int(t)
                     i=0
                     t=0
-        print(temp,light)
+        
         lcd.clear()
         lcd.message('Capturing\nImage')
         stream = os.popen('fswebcam /home/pi/test/Images/image.jpg')
@@ -92,6 +132,7 @@ while True:
 
         with open("/home/pi/test/Images/image.jpg", "rb") as img_file:
             my_string = base64.b64encode(img_file.read())
+        
         lcd.clear()
         lcd.message('Temp: ' + str(temp) + "\nLight: " + str(light))
         querystring = "http://"+ ip + ":5000/?usage=newpoint&image="
@@ -103,6 +144,8 @@ while True:
         querystring+="&number=1"
         response = requests.get(querystring)
         time.sleep(3.0)
+        
         lcd.clear()
         lcd.message("Response\n"+response.text)
         time.sleep(3.0)
+        rfid = ""
