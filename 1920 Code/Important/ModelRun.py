dbec4c1831e0705fe25e61c8360a61401f4b1bc6
Arya Tschand <43081871+aryatschand@users.noreply.github.com>
2021-06-28 13:14:08 -0400

    Update ModelRun.py

--- 1920 Code/Important/ModelRun.py
+++ 1920 Code/Important/ModelRun.py
@@ -1,62 +1,138 @@
-# Import TF libraries and data query files
 import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-import GetIdealColor # Ideal color loaded from database
-import getIrrigation # Irrigation volumes loaded from database
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import timedelta
+from sklearn.preprocessing import RobustScaler
+import sklearn.model_selection as skl
+plt.style.use("bmh")
 
-# Create data sets from data query functions from database
-# Currently, the model is formatted for consistent data input/output from database
-# To format model for environmental data from NASA PhenoCam, switch data functions to csv file reads
-(x_train, y_train), (x_test, y_test) = getIrrigation.getIrrigation(), GetIdealColor.idealColor()
-x_train, x_test = x_train / 255.0, x_test / 255.0
-sample, sample_label = x_train[0], y_train[0]
+# Technical Analysis library
+import ta
 
-# Create model
-model = keras.Sequential()
+# Neural Network library
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout
 
-# Input Layer
-model.add(layers.Embedding(input_dim=1000, output_dim=64))
+# Loading in the Data
+df = getIrrigation.getIrrigation(), GetIdealColor.idealColor()
 
-# LSTM Layer
-model.add(layers.LSTM(128))
 
-# Dense Layer
-model.add(layers.Dense(10))
+## Datetime conversion
+df['Date'] = pd.to_datetime(df.Date)
 
-# Bidirectional layers for LSTM backprop
-model.add(
-    layers.Bidirectional(layers.LSTM(64, return_sequences=True), input_shape=(5, 10))
-)
-model.add(layers.Bidirectional(layers.LSTM(32)))
-model.add(layers.Dense(10))
+# Setting the index
+df.set_index('Date', inplace=True)
 
-# Build the RNN model
-def build_model(allow_cudnn_kernel=True):
+# Dropping any NaNs
+df.dropna(inplace=True)
 
-    units = 64
-    input_dim = 28
-    output_size = 10    
+## Scaling
 
-    if allow_cudnn_kernel:
-        # Default RNN LSTM Layer
-        lstm_layer = keras.layers.LSTM(units, input_shape=(None, input_dim))
-    else:
-        lstm_layer = keras.layers.RNN(
-            keras.layers.LSTMCell(units), input_shape=(None, input_dim)
-        )
-    model = keras.models.Sequential(
-        [
-            lstm_layer,
-            keras.layers.BatchNormalization(),
-            keras.layers.Dense(output_size),
-        ]
-    )
+# Scale fitting the close prices separately for inverse_transformations purposes later
+close_scaler = RobustScaler()
+close_scaler.fit(df[['Close']])
 
-    # Fit the model on training data and return model
-    model.fit(x_train, y_train)
+# Normalizing/Scaling the DF
+scaler = RobustScaler()
+df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns, index=df.index)
 
-    return model
+def split_sequence(seq, n_steps_in, n_steps_out):
+    """
+    Splits the multivariate time sequence
+    """
+    
+    # Creating a list for both variables
+    X, y = [], []
+    
+    for i in range(len(seq)):
+        
+        # Finding the end of the current sequence
+        end = i + n_steps_in
+        out_end = end + n_steps_out
+        
+        # Breaking out of the loop if we have exceeded the dataset's length
+        if out_end > len(seq):
+            break
+        
+        # Splitting the sequences into: x = past prices and indicators, y = prices ahead
+        seq_x, seq_y = seq[i:end, :], seq[end:out_end, 0]
+        
+        X.append(seq_x)
+        y.append(seq_y)
+    
+    return np.array(X), np.array(y)
+    
+def layer_maker(n_layers, n_nodes, activation, drop=None, d_rate=.5):
+    """
+    Creates a specified number of hidden layers for an RNN
+    Optional: Adds regularization option - the dropout layer to prevent potential overfitting (if necessary)
+    """
+    
+    # Creating the specified number of hidden layers with the specified number of nodes
+    for x in range(1,n_layers+1):
+        model.add(LSTM(n_nodes, activation=activation, return_sequences=True))
 
+        # Adds a Dropout layer after every Nth hidden layer (the 'drop' variable)
+        try:
+            if x % drop == 0:
+                model.add(Dropout(d_rate))
+        except:
+            pass
+          
+    # How many periods looking back to learn
+n_per_in  = 200
+# How many periods to predict
+n_per_out = 30
+# Features 
+n_features = df.shape[1]
+# Splitting the data into appropriate sequences
+X, y = split_sequence(df.to_numpy(), n_per_in, n_per_out)
+
+#print(X, y)
+## Creating the NN
+
+# Instatiating the model
+model = Sequential()
+
+# Activation
+activ = "tanh"
+
+# Input layer
+model.add(LSTM(90, 
+               activation=activ, 
+               return_sequences=True, 
+               input_shape=(n_per_in, n_features)))
+
+# Hidden layers
+layer_maker(n_layers=1, 
+            n_nodes=30, 
+            activation=activ)
+
+# Final Hidden layer
+model.add(LSTM(60, activation=activ))
+
+# Output layer
+model.add(Dense(n_per_out))
+
+# Model summary
+model.summary()
+
+# Compiling the data with selected specifications
+model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+## Fitting and Training
+res = model.fit(X, y1, epochs=50, batch_size=128, validation_split=0.1)
+
+yhat = model.predict(np.array(df.tail(n_per_in)).reshape(1, n_per_in, n_features))
+# Transforming the predicted values back to their original format
+yhat = close_scaler1.inverse_transform(yhat)[0]
+print(yhat)
+# Creating a DF of the predicted prices
+preds = pd.DataFrame(yhat, 
+                     index=pd.date_range(start=df.index[-1]+timedelta(days=1), 
+                                         periods=len(yhat), 
+                                         freq="B"), 
+                     columns=[df1.columns[0]])
+
+# Printing the predicted prices
+print(preds)
 
